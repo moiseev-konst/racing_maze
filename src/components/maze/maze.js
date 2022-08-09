a13cc1761d4770af6bc2b7a35dba1545e2de3949
Kostya <moiseev.konst@gmail.com>
2022-08-09 17:58:29 +0300

    init racingMaze

---
+++ src/components/maze/maze.js
@@ -0,0 +1,5 @@
+
+export default function Maze(props){
+
+    return (<div className='maze' > MAZE</div>)
+}
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Maze from './components/maze/maze';
+import './components/maze/maze.css'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Maze/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
