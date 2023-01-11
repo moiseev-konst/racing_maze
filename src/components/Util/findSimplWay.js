d451bf439e5449f591af8fc5ed8142b2a10ac779
Kostya <moiseev.konst@gmail.com>
2023-01-11 18:55:01 +0300

    findWay some done

--- src/components/Util/findSimplWay.js
+++ src/components/Util/findSimplWay.js
@@ -1,60 +1,91 @@
-import possibleMove from "./possibleMove"
+import possibleMove from "./possibleMove";
 import checkMazeBorder from "./checkMazeBorder";
 
-export default function findSimplWay(mousePosition, cheesePosition, maze, size) {
-    const arrModelMaze = Array(maze.length).fill(0)
-    arrModelMaze[mousePosition] = 1
-    arrModelMaze[cheesePosition] = -1
-    let memoryArr = []
-    const way = []
-    let stepIteration = 1;
-    memoryArr.push(mousePosition)
+export default function findSimplWay(
+  mousePosition,
+  cheesePosition,
+  maze,
+  size
+) {
+  const arrModelMaze = Array(maze.length).fill(null);
+  arrModelMaze[mousePosition] = 0;
+  arrModelMaze[cheesePosition] = -1;
+  let memoryArr = [];
+  const way = [];
+  let stepIteration = 1;
+  memoryArr.push(mousePosition);
 
-    function checkCells(index) {
-        return arrModelMaze[index]
+  function checkCells(index) {
+    return arrModelMaze[index];
+  }
+
+  //function filterPosiblMove(position) {}
+
+  function findWay(mousePosition) {
+    let frontWave = [mousePosition];
+
+    let arr = [];
+
+    function isEnd(arr) {
+      return arr.some((el) => {
+       
+        return arrModelMaze[el] !== -1;
+      });
     }
 
-    function filterPosiblMove(position) {
+    function recordStep(cells) {
+      let posMove = possibleMove(cells, size.column);
 
+      let check = posMove.filter((el) => {
+      return arrModelMaze[el.index]!==0&&arrModelMaze[el.index]===null
+        // return !checkCells(el.index);
+      });
+
+      let filterCells = check.filter((el) => {
+        return !checkMazeBorder(el.move, maze, cells);
+      });
+
+      filterCells.forEach((el) => {
+        arrModelMaze[el.index] = stepIteration;
+        arr.push(el.index);
+      });
     }
-
-    function findWay(memoryArr) {
-
-        for (let i = 0; i < memoryArr.length; i++) {
-
-            let posMove = possibleMove(memoryArr[i], size.column)
-            let check = posMove.filter((el) => { return !checkCells(el.index) })
-            let filterCells = check.filter((el) => {
-                return !checkMazeBorder(el.move, maze, el.index)
-            })
-            memoryArr = [...memoryArr, ...filterCells]
-            console.log(posMove)
-            console.log(check)
-            console.log(filterCells)
+let it=0
+    while (isEnd(frontWave)) {
+        frontWave.forEach((el)=>{recordStep(el)})
+        frontWave=arr
+        arr=[]
+        stepIteration = stepIteration + 1;
+        console.log('Start');
+        it++
+        if(it>500){
+            break
         }
+    }
+    console.log('End');
+    console.log(frontWave);
+    }
 
+    /* 
+    for (let i = 0; i < frontWave.length; i++) {
+      memoryArr = [...memoryArr, ...filterCells];
+      console.log(posMove);
+      console.log(check);
+      console.log(filterCells);
+    }
 
-
-        /* filterCells.forEach((el) => {
+    filterCells.forEach((el) => {
              memoryArr.push(el.index)
              arrModelMaze[el.index] = stepIteration + 1
 
          })*/
 
-        stepIteration = stepIteration + 1
+    
+  
+  findWay(mousePosition);
+  console.log(arrModelMaze);
+  console.log(memoryArr);
+  console.log(way);
 
-
-
-
-    }
-    findWay(mousePosition)
-    console.log(arrModelMaze)
-    console.log(memoryArr)
-    console.log(way)
-
-
-
-
-    return way
-
+  return way;
 }
